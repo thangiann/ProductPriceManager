9c79a54f7f7067eda22ac8b08452ec0745fe94ea
thangiann <thanogiannopoulos13@gmail.com>
2025-11-18 17:32:29 +0200

    Year model without MeasurementDTO

--- src/main/java/models/Year.java
+++ src/main/java/models/Year.java
@@ -2,6 +2,33 @@ package models;
 
 import java.util.ArrayList;
 
+import dto.YearDTO;
+
 public class Year {
 
+    private int year;
+    private Top10 top10;
+
+    public Year(int year){
+        this.year = year;
+        this.top10 = new Top10(year);
+    }
+
+    public YearDTO createYearDTO(){
+
+        ArrayList<String> headlines = getTop10Headlines();
+        ArrayList<String> aliases = getTop10Aliases();
+
+        return new YearDTO(year, null, aliases, headlines);
+    }
+
+
+    //helper methods
+    private ArrayList<String> getTop10Headlines(){
+        return top10.top10Headlines();
+    }
+
+    private ArrayList<String> getTop10Aliases(){
+        return top10.top10Aliases();
+    }
 }
